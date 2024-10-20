dcaeca3175760aa3eda8e06d188067b2fe80ce9c
MatthewAlbisser <144923804+MatthewAlbisser@users.noreply.github.com>
2024-10-20 16:09:03 -0400

    Forgotten comments. Will finish when I get back from work.

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ enum TileType : int
     GRASS,      // Marks unoccupied space, can be overwritten 
     DIRT,       // Marks the path, cannot be overwritten
     WAYPOINT,   // Marks where the path turns, cannot be overwritten
-    TURRET,         // [HW3] New turret tiletype.
+    TURRET,         // [HW3] New tiletype named TURRET, will be called with 3.
     COUNT
 };
 
@@ -99,21 +99,22 @@ std::vector<Cell> FloodFill(Cell start, int tiles[TILE_COUNT][TILE_COUNT], TileT
     return result;
 }
 
-struct Enemy        // [HW3]
+struct Enemy        // [HW3] Struct for the enemies
 {
     size_t curr = 0;
     size_t next = curr + 1;
+
     Vector2 position{};
     int health = 10;
     bool atEnd = false;
 };
 
-struct Turret       // [HW3]
+struct Turret       // [HW3] Struct for the turrets
 {
     Vector2 position{};
     float range = 250.0f;
     float rateOfFire = 1.0f;
-    int damage = 10;
+    int damage = 10;            // -!!- Applying damage to enemy was crashing the program.
     float currentCDT = 0.0f;
 };
 
@@ -153,130 +154,132 @@ int main()
     };
 
     std::vector<Cell> waypoints = FloodFill({ 0, 12 }, tiles, WAYPOINT);
-    std::vector<Enemy> enemies;     // [HW3]
-    std::vector<Turret> turrets;    // [HW3]
+    std::vector<Bullet> bullets;
+    std::vector<Enemy> enemies;     // [HW3] Creates vector to hold number of enemies.
+    std::vector<Turret> turrets;    // [HW3] Creates vector to hold number of turrets.
 
-    for (int row = 0; row < TILE_COUNT; ++row)      // [HW3] for each row.
-    {
-        for (int col = 0; col < TILE_COUNT; ++col)  // [HW3] and for each column.
-        {
-            if (tiles[row][col] == 3)   // [HW3] If the tile is set to 3.
-            {
-                Turret turret;
-                turret.position = TileCenter(row, col);
-                turrets.push_back(turret);
-            }
-        }
-    }
-
-    //Vector2 enemyPosition = TileCenter(waypoints[curr].row, waypoints[curr].col); // [HW3] Commented out
+    // -- ENEMY VARIABLES ------------
     const float enemySpeed = 250.0f;
     const float enemyRadius = 20.0f;
 
+    float enemyCDT = 0.0f;      // [HW3]    Enemy spawn cooldown timer.
+    float spawnStall = 1.0f;    // [HW3]    Time between enemy spawns.
+    int enemySpawned = 0;       // [HW3]    Current count of enemies spawned.
+    int enemyTotal = 10;        // [HW3]    Enemy count limit to enemy spawning.
+
+    // -- BULLET VARIABLES ------------
     const float bulletTime = 1.0f;
     const float bulletSpeed = 500.0f;
     const float bulletRadius = 15.0f;
 
-    std::vector<Bullet> bullets;
+    // -- TURRET VARIABLES ------------
     float shootCurrent = 0.0f;
     float shootTotal = 0.25f;
 
-    float enemyCDT = 0.0f;      // [HW3]    Enemy spawn cooldown timer.
-    float spawnStall = 1.0f;    // [HW3]    Time between enemy spawns.
-    int enemySpawned = 0;       // [HW3]    Current number of enemies spawned.
-    int enemyTotal = 10;        // [HW3]    Enemy count limit to stop enemy spawning.
+    for (int row = 0; row < TILE_COUNT; ++row)      // [HW3] for each row...
+    {
+        for (int col = 0; col < TILE_COUNT; ++col)  // [HW3] and for each column...
+        {
+            if (tiles[row][col] == 3)               // [HW3] If the tile is equal to 3...
+            {
+                Turret turret;                              // [HW3] Apply struct data to turret variable.
+                turret.position = TileCenter(row, col);     // [HW3] Place turret in the center of pre-determined tile position. 
+                turrets.push_back(turret);                  // [HW3] Creates a space and adds turret value to the end of the vector.  
+            }
+        }
+    }
 
     InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Tower Defense");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        float dt = GetFrameTime();  // [HW3]   
+        float dt = GetFrameTime(); // Delta time variable.
 
         Vector2 mouse = GetMousePosition();     // [A1]    Mouse varible set from GetMousePosition.
         Cell mouseCell;                         // [A1]    Struct variable set to mouseCell.
         mouseCell.col = mouse.x / TILE_SIZE;    // [A1]    Column equals X-axis pixel position divided by tilesize to set tile X-coord. 
         mouseCell.row = mouse.y / TILE_SIZE;    // [A1]    Row equals Y-axis pixel position divided by tilesize to set tile Y-coord. 
- 
-        enemyCDT += dt;             // [HW3]
 
-        // [hw3] Enemy Spawning
-        if (enemyCDT >= spawnStall && enemySpawned < enemyTotal)    // [HW3] If enemy cool down is greater or equal to spawn stall time (1 second) and total spawned enemies is less then or equal to total enemies (10 units)...
-        {                                                           // [HW3] Create enemy, set position, set direction, set health, reset cool down, add 1 to total enemies, places enemy into vector.
-            Enemy enemy;
-            enemySpawned++;
-            enemy.position = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
-            enemy.next = 1;
-            enemy.health = 1;
-            enemy.atEnd = false;
-            enemies.push_back(enemy);
-            enemyCDT = 0.0f;
+        // -- ENEMY SPAWNING ---------------------------------
+        enemyCDT += dt;     // [HW3] Enemy spawn cool down
+
+        if (enemyCDT >= spawnStall && enemySpawned < enemyTotal)    // [HW3] If enemy cool down is greater or equal to spawn stall time (1 second)
+        {                                                           // and total spawned enemies is less then or equal to total enemies (10 units)...
+            Enemy enemy;                                            // [HW3] Create enemy from struct,
+            enemySpawned++;                                         // [HW3] Adds 1 to total enemy variable,
+            enemy.position = TileCenter(waypoints[enemy.curr].row,  // [HW3] Set row position,
+                waypoints[enemy.curr].col);                         // [HW3] Set column position,
+            enemy.next = 1;                                         // [HW3] Sets next waypoint,
+            enemies.push_back(enemy);                               // [HW3] Places enemy at end of vector,
+            enemyCDT = 0.0f;                                        // [HW3] Reset cool down.
         }
 
-        // Path following
-        for (Enemy& enemy : enemies)    // [HW3] Referencing the enemy struct with
+        // -- ENEMY PATH FOLLOWING ---------------------------------
+        for (Enemy& enemy : enemies)        // [HW3]  For each enemy in enemies vector...
         {
-            if (!enemy.atEnd)
+            if (!enemy.atEnd)               // [HW3] If an enemy has not reached the end...
             {
-                Vector2 from = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
-                Vector2 to = TileCenter(waypoints[enemy.next].row, waypoints[enemy.next].col);
-                Vector2 direction = Normalize(to - from);
-                enemy.position = enemy.position + direction * enemySpeed * dt;
-                if (CheckCollisionPointCircle(enemy.position, to, enemyRadius))
+                Vector2 from = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);    // [HW3] Set variable for previous waypoint
+                Vector2 to = TileCenter(waypoints[enemy.next].row, waypoints[enemy.next].col);      // [HW3] Set varibable for next waypoint
+                Vector2 direction = Normalize(to - from);                                           // [HW3] Set variable for direction using both "To" and "From" variable vectors. 
+                enemy.position = enemy.position + direction * enemySpeed * dt;                  // [HW3] Calculates enemy speed.
+                if (CheckCollisionPointCircle(enemy.position, to, enemyRadius))                 // [HW3] If enemy circle is touching or overlapping with the next waypoint...
                 {
-                    enemy.curr++;
-                    enemy.next++;
+                    enemy.curr++;                                       // [HW3] Add 1 to enemies current waypoint, making its previous destination the start.
+                    enemy.next++;                                       // [HW3] 
                     enemy.atEnd = enemy.next == waypoints.size();
                     enemy.position = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
                 }
             }
         }
-
-        for (Turret& turret : turrets)
+        // -- TURRET TARGETING --------------------------------------------
+        for (Turret& turret : turrets)      // [HW3] For every turret in the vector spawned...
         {
-            turret.currentCDT += dt;
-            Enemy* targets = nullptr;
-            for (Enemy& enemy : enemies)
+            turret.currentCDT += dt;            // [HW3] Increase its current cool down float in real time. 
+            Enemy* targets = nullptr;           // [HW3] Creates targets pointer for Enemies. points to null on start, preventing issues.
+            for (Enemy& enemy : enemies)        // [HW3] For every enemy in the vector spawned...
             {
-                float distance = Distance(turret.position, enemy.position);
-                if (distance < turret.range)
+                float distance = Distance(turret.position, enemy.position);         // [HW3] Create variable for distance between a turret and an enemy.
+                if (distance < turret.range)                                        // [HW3] If current distance is shorter then turrets max range...
                 {
-                    targets = &enemy;
+                    targets = &enemy;                                               // [HW3] Enemies become targeted. 
                 }
             }
-            if (targets && turret.currentCDT >= turret.rateOfFire)
+            if (targets && turret.currentCDT >= turret.rateOfFire)                  // [HW3] If target enemies exist AND turret cool down passes the rate of fire.
             {
-                turret.currentCDT = 0.0f;
-                Bullet bullet;                                                      // [hw3] Creates bullet from bullet struct. Moved this chunk from if shooting statement bellow
-                bullet.position = turret.position;                                  // [HW3] Changed to Turret position
-                bullet.direction = Normalize(targets->position - bullet.position);
-                bullets.push_back(bullet);
+                turret.currentCDT = 0.0f;                                           // [HW3] Reset turret cool down.
+                Bullet bullet;                                                      // [HW3] Creates bullet from bullet struct. (Moved this chunk from existing code)
+                bullet.position = turret.position;                                  // [HW3] Bullet position starts on the active turret position.
+                bullet.direction = Normalize(targets->position - bullet.position);  // [HW3] Aims bullet at target using the target pointer direction and distance.
+                bullets.push_back(bullet);                                          // [HW3] Place new bullet on the end of bullets vector.
             }
         }
 
-        // Bullet update
-        for (Bullet& bullet : bullets)
+        // -- BULLET MOVEMENT ---------------------------------------
+        for (Bullet& bullet : bullets)                                                  
         {
-            bullet.position = bullet.position + bullet.direction * bulletSpeed * dt;    // aiming
-            bullet.time += dt;                                                          // time simulation 
+            bullet.position = bullet.position + bullet.direction * bulletSpeed * dt;    
+            bullet.time += dt;                                                          
+            bool expired = bullet.time >= bulletTime;                                   
 
-            bool expired = bullet.time >= bulletTime;                                   // remove bullet
-            for (int i = 0; i < enemies.size();)
+            for (int i = 0; i < enemies.size();)         // [HW3] For one enemy in the vector starting at 0.                                                                   
             {
-                Enemy& enemy = enemies[i];
-                bool collision = CheckCollisionCircles(enemy.position, enemyRadius, bullet.position, bulletRadius);
+                Enemy& enemy = enemies[i];                                  // [HW3]
+                bool collision = CheckCollisionCircles(enemy.position,      // [HW3]
+                    enemyRadius, bullet.position, bulletRadius);            // [HW3]
                 if (collision)
                 {
-                    enemy.health--;
-                    if (enemy.health <= 0)
+                    enemy.health--;                                         // [HW3]
+                    if (enemy.health <= 0)                                  // [HW3]
                     {
-                        enemies.erase(enemies.begin() + i);
-                        bullet.enabled = false;
-                        break;
+                        enemies.erase(enemies.begin() + i);                 // [HW3]
+                        bullet.enabled = false;                             // [HW3]
+                        break;                                              // [HW3]
                     }
                 }
                 else
                 {
-                    i++;
+                    i++;    // [HW3] Add one to forloop. 
                 }
             }
             bullet.enabled = !expired && bullet.enabled;
@@ -288,6 +291,8 @@ int main()
                 return !bullet.enabled;
             }), bullets.end());
 
+        // -- RENDERING ---------------------------------
+
         BeginDrawing();
         ClearBackground(BLACK);
         for (int row = 0; row < TILE_COUNT; row++)
@@ -298,16 +303,18 @@ int main()
             }
         }
 
-        // Render
-        for (const Enemy& enemy : enemies)
+        for (const Enemy& enemy : enemies)                          // [HW3] Draw enemies when spawned from vector.
             DrawCircleV(enemy.position, enemyRadius, RED);
-        for (const Turret& turret : turrets)
+
+        for (const Turret& turret : turrets)                        // [HW3] Draw turrets, not simple to change them to squares so they're staying as circles.
             DrawCircleV(turret.position, enemyRadius, DARKPURPLE);
+
         for (const Bullet& bullet : bullets)
             DrawCircleV(bullet.position, bulletRadius, BLUE);
-        DrawText(TextFormat("Total bullets: %i", bullets.size()), 10, 10, 20, BLUE);
-        DrawTile(mouseCell.row, mouseCell.col, PURPLE);
 
+        DrawText(TextFormat("Total bullets: %i", bullets.size()), 10, 10, 20, BLUE);
+
+        DrawTile(mouseCell.row, mouseCell.col, SKYBLUE);            // [A1] Draw mouse position tile with sky blue colour.
         EndDrawing();
     }
     CloseWindow();
